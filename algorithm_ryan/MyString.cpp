6b747fcc96d9b286373078497ddb7c687c774249
ForrestRyan <53398721+ForrestRyan@users.noreply.github.com>
2022-12-27 11:34:43 +0800

    Update MyString.cpp
    [Modify] check if endl is an object, Build-in data type or function.

--- algorithm_ryan/MyString.cpp
+++ algorithm_ryan/MyString.cpp
@@ -1,7 +1,24 @@
 #include "MyString.h"
 
+ostream& endl(ostream& out) // manipulator
+{
+    out.put('\n');
+    out.flush();
+    return out;
+}
+
+
+// ostream& ostream::operator<<(ostream& (*func)(ostream& ))
+// {
+//     return (*func)(*this);
+// }
+
 int main()
 {
+    cout << "hello world" << endl; //endl: '\n' and flush
+    // endl is an OBJECT? Build-in data type or function? Answer is function!
+
+
     MyString mystr("hello world");
     cout << mystr;
 
