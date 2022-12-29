132c90a2759f2ec8b9534a648c8083d8f0364e63
Ryan <dut2016ryan@126.com>
2022-12-29 21:55:01 +0800

    [Add] test how to use move constructor

--- algorithm_ryan/MyString.cpp
+++ algorithm_ryan/MyString.cpp
@@ -13,17 +13,27 @@ ostream& endl(ostream& out) // manipulator
 //     return (*func)(*this);
 // }
 
+MyString getString(const char* str)
+{
+    MyString tmp(str); //default constructor
+    return tmp; // move constructor
+}
+
 int main()
 {
     cout << "hello world" << endl; //endl: '\n' and flush
     // endl is an OBJECT? Build-in data type or function? Answer is function!
 
-
-    MyString mystr("hello world");
-    cout << mystr;
+    MyString mystr0("hello"); // default constructor
+    MyString mystr1(mystr0); // copy constructor 
+    MyString mystr2 = mystr0; // copy constructor 
+    mystr2 = mystr0; //copy assignement
+    MyString mystr3(getString("hello world")); // include move constructor, but due to compile optimization, we cant see the process
+    MyString mystr4 = std::move(mystr0); // move constructor
+    cout << mystr1 << mystr2 << mystr3 << mystr4;
 
     cout << endl;
-    cin>>mystr;
-    cout << mystr;
+    cin>>mystr4;
+    cout << mystr4;
     return 0;
 }
--- algorithm_ryan/MyString.h
+++ algorithm_ryan/MyString.h
@@ -5,9 +5,10 @@ using namespace std;
 class MyString
 {
 public:
+    //default constructor: deep copy
     explicit MyString(const char* str)
     {
-        cout << "constructor is called." << endl;
+        cout << "default constructor is called." << endl;
         if(nullptr == str)
             str = "";
 
@@ -15,14 +16,26 @@ public:
         strcpy_s(m_str, strlen(str)+1, str);
     }
 
+    //copy constructor: deep copy
     MyString(const MyString& str)
     {
+        cout << "copy constructor is called." << endl;
         m_str = new char[strlen(str.m_str) + 1];
         strcpy_s(m_str, strlen(str.m_str)+1, str.m_str);
     }
 
+    //move constructor: shallow copy
+    MyString(MyString&& str)
+    {
+        cout << "move constructor is called." << endl;
+        m_str = str.m_str;
+        str.m_str = nullptr;
+    }
+
+    //copy assignment: deep copy
     MyString& operator=(const MyString& str)
     {
+        cout << "copy assignment is called." << endl;
         if(this == &str)
             return *this;
         
@@ -34,6 +47,7 @@ public:
 
     ~MyString()
     {
+        cout << "destructor is called." << endl;
         if(m_str != nullptr)
         {
             delete []m_str;
@@ -41,20 +55,20 @@ public:
         }
     }
 
+    //operator override
     friend ostream& operator<<(ostream& out, const MyString& str)
     {
         out << "MyString is output: " << str.m_str << endl;
         return out;
     }
 
+    //operator override
     friend istream& operator>>(istream& in, const MyString& str)
     {
         cout << "MyString is waiting for input from keyboard: ";
         in >> str.m_str;
         return in;
     }
-
-
 
 private:
     char* m_str;
