dc441ee82bbccde5be0dde60bc8ea855a0bf98de
Ryan <dut2016ryan@126.com>
2023-01-02 13:15:46 +0800

    [Opt] optimize the implementation and add how to use the template to find specific element.

--- algorithm_ryan/MyString.cpp
+++ algorithm_ryan/MyString.cpp
@@ -21,19 +21,34 @@ MyString getString(const char* str)
 
 int main()
 {
-    cout << "hello world" << endl; //endl: '\n' and flush
-    // endl is an OBJECT? Build-in data type or function? Answer is function!
+    cout << "hello world" << endl; //endl: '\n' and flush   // endl is an OBJECT? Build-in data type or function? Answer is function!
 
-    MyString mystr0("hello"); // default constructor
-    MyString mystr1(mystr0); // copy constructor 
-    MyString mystr2 = mystr0; // copy constructor 
-    mystr2 = mystr0; //copy assignement
-    MyString mystr3(getString("hello world")); // include move constructor, but due to compile optimization, we cant see the process
-    MyString mystr4 = std::move(mystr0); // move constructor
-    cout << mystr1 << mystr2 << mystr3 << mystr4;
+    MyString mystr_mv0("MOVE_0_TEST"); // default constructor
+    MyString mystr_mv1("MOVE_1_TEST"); // default constructor
+    MyString mystr("DEFAULT_TEST"); // default constructor
 
+    MyString mystr1(mystr); // copy constructor 
+    MyString mystr4 = mystr; // copy constructor 
+    // MyString mystr2(getString("hello world")); // include move constructor, but due to compile optimization, we cant see the process
+    MyString mystr3 = std::move(mystr_mv1); // move constructor
+
+    mystr4 = mystr; //copy assignement
+    mystr4 = static_cast<MyString&&>(mystr_mv0); //move assignment
+
+    cout << mystr1 << mystr3 << mystr4;
+
+#if 0
     cout << endl;
     cin>>mystr4;
     cout << mystr4;
+#endif
+
+    char element = 'T';
+    char* iter = mystr.find<char*, char>(mystr.m_data, mystr.m_data+ strlen(mystr.m_data), element);
+    if(*iter != '\0')
+        cout << "MyString::find: the element " << *iter  << " is found" << endl << endl;
+    else
+        cout << "MyString::find: the element " << *iter  << " is not found! please check it." << endl << endl;
+
     return 0;
 }
--- algorithm_ryan/MyString.h
+++ algorithm_ryan/MyString.h
@@ -5,80 +5,132 @@ using namespace std;
 class MyString
 {
 public:
+    MyString()
+    : m_data(nullptr)
+    {
+
+    }
+
     //default constructor: deep copy
     explicit MyString(const char* str)
     {
-        cout << "default constructor is called." << endl;
+        cout << "default constructor is called." << " str: " << str << endl;
         if(nullptr == str)
             str = "";
 
-        m_str = new char[strlen(str) + 1];
-        strcpy_s(m_str, strlen(str)+1, str);
+        size_t len = strlen(str);
+        m_data = new char[len + 1];
+        // strcpy_s(m_data, strlen(str)+1, str);
+        // strcpy(m_data, str);
+        memcpy(m_data, str, len);
+        m_data[len+1] = '\0';
     }
 
     //copy constructor: deep copy
-    MyString(const MyString& str)
+    MyString(const MyString& src)
     {
-        cout << "copy constructor is called." << endl;
-        m_str = new char[strlen(str.m_str) + 1];
-        strcpy_s(m_str, strlen(str.m_str)+1, str.m_str);
+        cout << "copy constructor is called." << " str: " << src.m_data << endl;
+        m_data = new char[strlen(src.m_data) + 1];
+        strcpy_s(m_data, strlen(src.m_data)+1, src.m_data);
     }
 
 #if 0
     //move constructor: shallow copy
-    MyString(MyString&& str)
+    MyString(MyString&& src)
     {
         cout << "move constructor is called." << endl;
-        m_str = str.m_str;
-        str.m_str = nullptr;
+        m_data = src.m_data;
+        src.m_data = nullptr;
     }
 #endif
 
     //move constructor: shallow copy
-    MyString(MyString&& str)
-    :m_str(std::move(str.m_str))
+    MyString(MyString&& src)
+    :m_data(std::move(src.m_data))
     {
-        cout << "move constructor is called." << endl;
+        cout << "move constructor is called." << " str: " << src << endl;
     }
 
     //copy assignment: deep copy
-    MyString& operator=(const MyString& str)
+    MyString& operator=(const MyString& src)
     {
-        cout << "copy assignment is called." << endl;
-        if(this == &str)
+        cout << "copy assignment is called." << " str: " << src << endl;
+        if(this == &src) // if both objects are the same, return directly
+            return *this;
+
+        if(m_data) // if current data is not empty, clear it first.
+        {
+            delete m_data;
+            m_data = nullptr;
+        }
+        
+        m_data = new char[strlen(src.m_data) + 1];
+        strcpy_s(m_data, strlen(src.m_data)+1, src.m_data);
+
+        return *this;
+    }
+
+    //copy assignment: shallow copy
+    MyString& operator=(MyString&& src)
+    {
+        cout << "move assignment is called." << " str: " << src << endl;
+        if(this == &src)
             return *this;
         
-        m_str = new char[strlen(str.m_str) + 1];
-        strcpy_s(m_str, strlen(str.m_str)+1, str.m_str);
+        if(m_data)
+        {
+            delete m_data;
+            m_data == nullptr;
+        }
+
+        m_data = src.m_data;
+        src.m_data = nullptr;
 
         return *this;
     }
 
     ~MyString()
     {
-        cout << "destructor is called." << endl;
-        if(m_str != nullptr)
+        cout << "destructor is called." << " str: " << m_data << endl;
+        if(m_data != nullptr)
         {
-            delete []m_str;
-            m_str = nullptr;
+            delete []m_data;
+            m_data = nullptr;
         }
     }
 
     //operator override
-    friend ostream& operator<<(ostream& out, const MyString& str)
+    friend ostream& operator<<(ostream& out, const MyString& src)
     {
-        out << "MyString is output: " << str.m_str << endl;
+        out << "MyString is output: " << src.m_data << endl;
         return out;
     }
 
     //operator override
-    friend istream& operator>>(istream& in, const MyString& str)
+    friend istream& operator>>(istream& in, const MyString& src)
     {
         cout << "MyString is waiting for input from keyboard: ";
-        in >> str.m_str;
+        in >> src.m_data;
         return in;
     }
 
-private:
-    char* m_str;
+    bool operator<(const MyString& rhs)
+    {
+        if(rhs.m_data == "")
+            return false;
+        
+        return std::string(this->m_data) < std::string(rhs.m_data);
+    }
+    
+    template<typename InputIterator, typename T>
+    InputIterator find(InputIterator first, InputIterator last, const T& value)
+    {
+        while (first != last && *first != value)
+            ++first;
+        
+        return first;
+    }
+
+public:
+    char* m_data;
 };
