9f1e0081132e7487a293690a7181e731fec5d949
ForrestRyan <53398721+ForrestRyan@users.noreply.github.com>
2023-02-20 16:00:43 +0800

    Update splitChineseWords-01.cpp

--- algorithm_ryan/splitChineseWords-01.cpp
+++ algorithm_ryan/splitChineseWords-01.cpp
@@ -15,7 +15,7 @@ bool isChineseChar(const char* utf8Str, int pos, unsigned int charLen) {
     }
     unsigned int high = std::pow(2, (8-charLen)) - 1;
     code |= (utf8Str[pos] & high) << (6 * (charLen - 1));
-    return code >= 0x4E00 && code <= 0x9FFF;
+    return (code >= 0x4E00 && code <= 0x9FFF || code >= 0x3400 && code <= 0x4DBF);
 }
 
 std::string insertBlank(const std::string& utf8Str) {
@@ -42,7 +42,7 @@ std::string insertBlank(const std::string& utf8Str) {
         }
 
         pos += charLen;
-        std::cout << "result: " << result << std::endl;
+        // std::cout << "result: " << result << std::endl;
     }
     auto first = result.find_first_not_of(" ");
     auto last = result.find_last_not_of(" ");
@@ -50,7 +50,7 @@ std::string insertBlank(const std::string& utf8Str) {
 }
 
 int main() {
-    std::string utf8Str = "你好world世hello";
+    std::string utf8Str = "我暫時操作唔到hello車內空氣123循環功能 肯德hello基颐123堤港店 朝阳区   ";
     std::cout << insertBlank(utf8Str) << std::endl;
     return 0;
 }
